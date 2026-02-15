80e7084fa3b2053ebcc97141947fed1e8d162ccc
Nikita <nikita.polischukov@gmail.com>
2026-02-16 00:11:49 +0100

    Fix dict formatting to checklist style

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ def format_linter_error(error: dict) -> dict:
         "message": error["text"],
         "column": error["column_number"],
         "name": error["code"],
-        "source": "flake8"
-    }
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed"
-    }
+        "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
