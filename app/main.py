1e47484667022914e7d58b2f0f6d38c83575f8d6
Nikita <nikita.polischukov@gmail.com>
2026-02-15 23:58:28 +0100

    Fix dict style formatting

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "message": error["text"],
-            "column": error["column_number"],
-            "name": error["code"],
-            "source": "flake8"
+    return {
+        "line": error["line_number"],
+        "message": error["text"],
+        "column": error["column_number"],
+        "name": error["code"],
+        "source": "flake8"
     }
-
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return { "errors": [format_linter_error(error) for error in errors],
-             "path": file_path,
-             "status": "failed" if errors  else "passed"
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
-
 
 
 def format_linter_report(linter_report: dict) -> list:
@@ -21,4 +21,3 @@ def format_linter_report(linter_report: dict) -> list:
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
-
